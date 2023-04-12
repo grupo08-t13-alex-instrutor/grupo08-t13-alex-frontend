da362129a9b89a5b11bd2608a2d13af86096d693
davi894 <moisesdavi005@gmail.com>
2023-04-12 07:58:27 -0300

    add: criando o componente Cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
 import GlobalStyle from './globalStyled'
-import './styled.css'
-GlobalStyle
+import { ThemeProvider } from 'styled-components';
+import theme from './themeProvider';
+import { Cards } from './components';
+
 function App() {
 
   return (
     <>
-      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <ul >
+          <Cards src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvEoM818AD-kqjm1GfBoLh-3349D6Fno0DHA&usqp=CAU"
+            marca="Mercedez"
+            descricao="Descrição do carro"
+            km="100.000 km"
+            name="aiasm"
+            ano="2020"
+            preco="R$ 50.000,00" />
+            
+          <Cards src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvEoM818AD-kqjm1GfBoLh-3349D6Fno0DHA&usqp=CAU"
+            marca="Mercedez"
+            descricao="Descrição do carro"
+            km="100.000 km"
+            name="aiasm"
+            ano="2020"
+            preco="R$ 50.000,00" />
+
+          <Cards src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvEoM818AD-kqjm1GfBoLh-3349D6Fno0DHA&usqp=CAU"
+            marca="Mercedez"
+            descricao="Descrição do carro"
+            km="100.000 km"
+            name="aiasm"
+            ano="2020"
+            preco="R$ 50.000,00" />
+        </ul>
+
+      </ThemeProvider>
     </>
   )
 
