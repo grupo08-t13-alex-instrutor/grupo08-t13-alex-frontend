b729b2439be3022983d37264a3817340cb509820
Leonardo Miranda do Nascimento <leonardo.miranda.3@hotmail.com>
2023-04-12 22:28:52 -0400

    Add: header criado

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,27 +3,86 @@ import { useState } from "react";
 import MotorsShop from "../../assets/MotorsShop.png";
 import { IoMenu, IoClose } from "react-icons/io5";
 
+interface iUser {
+    name: string,
+    description: string
+}
+
 export const Header = () => {
     const [openMenu, setOpenMenu] = useState(false);
+    const [userLogged, setUserLogged] = useState<null | iUser>(null);
+
+    let user: iUser = {
+        name: "Samuel Leão",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"
+    }
+
+    const { name, description } = user;
         
     return(
         <>
             <HeaderStyled>
                 <img src={MotorsShop} alt="Logo Motors Shop"/>
-                <button onClick={( event ) => {
+                <button className="invisible-options" onClick={( event ) => {
                     event.preventDefault();
                     setOpenMenu(!openMenu);
                 }}>
                     { openMenu ? <IoClose/> : <IoMenu/> }
                 </button>
+                <div>
+                    <hr />
+                    { userLogged ? 
+                            <button onClick={ event => {
+                                event.preventDefault();
+                                setUserLogged(null);
+                            }}>
+                                <div>
+                                    <span className="profile-picture">
+                                        <p>SL</p>
+                                    </span>
+                                    <p>{name}</p>
+                                </div>
+                            </button>
+                        :
+                            <>
+                                <button className="body-1-600 margin" onClick={ event => {
+                                    event.preventDefault();
+                                    setUserLogged(user);
+                                }}>
+                                    Fazer Login
+                                </button>
+                                <button className="button-big-text register margin">Cadastrar</button>
+                            </>
+                    }
+                </div>
             </HeaderStyled>
             <Menu topPosition={ openMenu ? "none" : "-400px" }>
-                <p>Carros</p>
-                <p>Motos</p>
-                <p>Leilão</p>
-                <hr />
-                <button>Fazer login</button>
-                <button>Cadastrar</button>
+                <div className={ openMenu ? "input" : "" }>
+                    <button className="body-1-600">Carros</button>
+                    <button className="body-1-600">Motos</button>
+                    <button className="body-1-600">Leilão</button>
+                    <hr />
+                    { userLogged ? 
+                            <>
+                                <button className="atention" onClick={ event => {
+                                    event.preventDefault();
+                                    setUserLogged(null);
+                                }}>
+                                    Logout
+                                </button>
+                            </>
+                        :
+                            <>
+                                <button className="body-1-600" onClick={
+                                    event => {
+                                        event.preventDefault();
+                                        setUserLogged(user);
+                                    }
+                                }>Fazer login</button>
+                                <button className="button-big-text register">Cadastrar</button>
+                            </>
+                    }
+                </div>
             </Menu>
         </>
     )
--- src/components/header/styled.ts
+++ src/components/header/styled.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 interface iMenuProps {
     topPosition: string
@@ -8,17 +8,19 @@ export const HeaderStyled = styled.header`
     width: 100%;
     min-width: 300px;
     padding: 25px 16px;
-    border-bottom: 5px solid gray;
-
+    border-bottom: 1px solid #ADB5BD;
     display: flex;
     justify-content: space-between;
-
     box-sizing: border-box;
+    z-index: 2;
+    position: relative;
+    background-color: white;
     
     button {
         padding: 0;
         border: none;
         background-color: white;
+        margin-left: auto;
 
         svg {
             width: 1.5rem;
@@ -26,31 +28,182 @@ export const HeaderStyled = styled.header`
         }
     }
 
-    .options {
-        z-index: 1;
+    div {
+        visibility: hidden;
+        width: 0;
+        height: 0;
+    }
+
+    @media (min-width: 720px) {
+        padding: 0 60px;
+
+        img {
+            padding: 26px 0;
+        }
+
+        .invisible-options {
+            visibility: hidden;
+            width: 0;
+        }
+
+        .body-1-600 {
+            font-style: normal;
+            font-weight: 600;
+            font-size: 16px;
+            line-height: 28px;
+            color: #495057;
+            text-align: start;
+        }
+        
+        .button-big-text {
+            font-style: normal;
+            font-weight: 600;
+            font-size: 16px;
+            color: #495057;
+        }
+
+        .register {
+            color: #0B0D0D;
+            padding: 12px 24px;
+            border: 1.5px solid #ADB5BD;
+            border-radius: 4px;
+        }
+
+        div {
+            width: auto;
+            height: auto;
+            max-height: 80px;
+            gap: 20px;
+            visibility: visible;
+            display: inline-flex;
+            
+            .margin {
+                margin: 16px 0;
+            }
+            
+            hr {
+                margin: 0;
+                color: #DEE2E6;
+            }
+            
+            div {
+                gap: 10px;
+                display: flex;
+                align-items: center;                
+            }
+
+            button {
+                margin: 0;
+            }
+            
+            button div .profile-picture {
+                width: 32px;
+                height: 32px;
+                border-radius: 50%;
+                background-color: #5126EA;
+                margin: auto 0;
+                padding: 0;
+
+                p {
+                    height: 32px;
+                    margin: 0;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    font-weight: 700;
+                    font-size: 14px;
+                    color: #ffffff;
+                }
+            }
+
+            div p {
+                font-style: normal;
+                font-weight: 400;
+                font-size: 16px;
+                line-height: 28px;
+                color: #495057;
+            }
+        }
     }
 `;
 
+
 export const Menu = styled.div<iMenuProps>`
+    @keyframes input {
+        0% {
+            transform: translateY( -400px );
+        }
+        100% {
+            transform: translateY( none );
+        }
+    }
+
+    .input {
+	    animation: input 1s normal;
+    }
+
     width: 100%;
-    padding: 32px 0;
-    gap: 40px;
-    box-sizing: border-box;
-
-    display: flex;
-    flex-direction: column;
-
-    background: white;
-    
     z-index: 1;
     position: absolute;
     top: ${ props => props.topPosition };
+    
+    div {
+        padding: 32px 0;
+        gap: 40px;
+        box-sizing: border-box;
+        background: white;
+        display: flex;
+        flex-direction: column;
+        
+        hr {
+            margin: 0;
+            color: #ADB5BD; 
+        }
+        
+        button {
+            margin: 0 16px;
+            border: none;
+            background-color: white;
+        }
 
-    p, hr {
-        margin: 0;
+        .atention {
+            padding: 12px 28px;
+            border: 1px solid #FDD8D8;
+            border-radius: 4px;
+            background-color: #FDD8D8;
+            color: #CD2B31;
+            font-size: 16px;
+            font-weight: 600;
+            font-style: normal;
+        }
+
+        .body-1-600 {
+            font-style: normal;
+            font-weight: 600;
+            font-size: 16px;
+            line-height: 28px;
+            color: #495057;
+            text-align: start;
+        }
+        
+        .button-big-text {
+            font-style: normal;
+            font-weight: 600;
+            font-size: 16px;
+            color: #495057;
+        }
+
+        .register {
+            color: #0B0D0D;
+            padding: 24px;
+            border: 1.5px solid #ADB5BD;
+            border-radius: 4px;
+        }
     }
 
-    p, button {
-        margin: 0 16px;
+    @media (min-width: 720px) {
+        visibility: hidden;
+        width: 0;
+        height: 0;
     }
 `;
